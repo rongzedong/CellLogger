71aaa0889bacaf604b571d895ac8195b7b07c999
QIAO Lun <qiaolun@gmail.com>
2012-04-30 22:22:37 +0800

    add a receiver

---
+++ src/net/qlun/celllogger/app/InitReceiver.java
@@ -0,0 +1,20 @@
+package net.qlun.celllogger.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InitReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+
+		if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+			// Intent i = new Intent(context, PhoneStateService.class);
+			// context.startService(i);
+		}
+
+	}
+
+}
